25992e393bddbc8277f0df5239d97816c76dc1bc
Ghamdi, Hamzah <ghamha1n@gmail.com>
2020-02-04 21:47:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require("express");
 
 // Constants
-const PORT = 8080;
-const HOST = "0.0.0.0";
+const PORT = process.env.PORT;
 
 // App
 const app = express();
@@ -11,4 +10,4 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT);
-console.log(`Running on http://${HOST}:${PORT}`);
+console.log(`Running on ${PORT}`);
