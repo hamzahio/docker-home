2f82cd532f83a5940784ede882d1f98f758dd686
Ghamdi, Hamzah <ghamha1n@gmail.com>
2020-02-05 09:08:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT;
 const app = express();
 app.get("/", (req, res) => {
   res.send(`
-  <h1 style="text-align:center;">Good Morning Hamzah Update Now Last Update then go to work</h1>
+  <h1 style="text-align:center;">Good Morning Hamzah (( Update Now Last Update then go to work ))</h1>
 
 `);
 });
