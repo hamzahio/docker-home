428b84bf014cd79712cc04bfd0442d288c03d913
Ghamdi, Hamzah <ghamha1n@gmail.com>
2020-02-07 01:30:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const app = express();
 app.get("/", (req, res) => {
   res.send(`
   <h1 style="text-align:center;">Good Morning Abduallh & 👩‍🍳  Hamzah good 👦</h1>
+  <pre>
+    <code>${JSON.stringify(process.env, null, 2)}</code>
+  </pre>
 
 `);
 });
