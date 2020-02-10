983f6f270ac1917a55db0633657dbb6942cc1673
Ghamdi, Hamzah <ghamha1n@gmail.com>
2020-02-10 23:02:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT;
 const app = express();
 app.get("/", (req, res) => {
   res.send(`
-  <h1 style="text-align:center;">Good Morning Musab & 👩‍🍳  Hamzah good 👦${process.env.MESSAGE}</h1>
+  <h1 style="text-align:center;">Good Morning ..... </h1>
   <pre>
     <code>${JSON.stringify(process.env, null, 2)}</code>
   </pre>
